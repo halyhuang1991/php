29a58e050cbe06d0b2722bac05d114783807591c
halyhuang1991 <18807898837@139.com>
2018-09-17 14:51:26 +0800

    1

--- test.php
+++ test.php
@@ -15,7 +15,18 @@ $db1 = F\subnamespace\Uni::getInstance("getInstance");
 echo $db1 -> getName().'<br/>';
 $a="asa";
 $b="asa";
-echo "Hello World!";
+echo "Hello World!"."<br/>";
+//读写文件
+$myfile = fopen("./Temp/test.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("./Temp/test.txt"))."<br/>";
+fclose($myfile);
+echo readfile("./Temp/test.txt");
+$myfile = fopen("./Temp/newfile.txt", "w") or die("Unable to open file!");
+$txt = "Bill Gates\n";
+fwrite($myfile, $txt);
+$txt = "Steve Jobs\n";
+fwrite($myfile, $txt);
+fclose($myfile);
 //http://localhost:9000
 //http://localhost:8082/dashboard/phpinfo.php
 ?>
